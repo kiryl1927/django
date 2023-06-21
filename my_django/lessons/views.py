1c0c712fadee3e97fc9dfd323a3a2d45ae4ac98c
kiryl1927 <ri6ik.kiril@mail.ru>
2023-06-21 21:19:03 +0300

    обновление

--- my_django/lessons/views.py
+++ my_django/lessons/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+
+def index(request):
+    context= {
+        "zap": "text1",
+        "zap2": "models.Model1.object.all()"
+
+    }
+    return render(request,"cat.html",context)
+
+def about(request):
+    return render(request,"about.html")
+
+def dogs (request):
+    return render(request,"dogs.html")
 
 # Create your views here.
